43cbf51c8ae0fd050e381225d679a8bba1fc31b3
천진아 <totter15@naver.com>
2023-06-15 20:47:02 +0900

    [Feat] : 홈 페이지 작성

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,15 @@
+import { Link } from 'react-router-dom';
+import './home.scss';
+
 function Home() {
-	return <div>Wanted Pre Onboarding Frontend</div>;
+	return (
+		<main className='home'>
+			<h1>Wanted Pre Onboarding Frontend</h1>
+
+			<Link to='signin'>로그인 하러가기</Link>
+			<Link to='signup'>회원가입 하러가기</Link>
+		</main>
+	);
 }
 
 export default Home;
